1d675e9d98bb02ffd966b30af8e937c1c88da08b
zhiminzhang0830 <452516515@qq.com>
2024-07-23 09:21:17 +0000

    update

--- structure_prediction/data/2d_structure/convert_to_csv.py
+++ structure_prediction/data/2d_structure/convert_to_csv.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import random
+from collections import defaultdict
 
 import pandas as pd
 import pymatgen
@@ -9,105 +10,92 @@ from pymatgen.core.structure import Structure
 from pymatgen.io.cif import CifParser
 
 csv_file = "./ehull_0621.csv"
-cif_path = "./cif_structure"
-save_path = "./2d_structure_csv_ehull_200"
+cif_path = "/root/host/ssd3/zhangzhimin04/workspaces_118/PP4Materials/structure_prediction/data_bak/paddle_project/data/2d_structure/cif_structure"
+save_path = "./2d_structure_csv_ehull_200_condition"
 os.makedirs(save_path, exist_ok=True)
+
+dist_file = "./mean_distances.csv"
+property_file = "./output_data.csv"
 
 filter_thresh = 1
 
 cif_names = os.listdir(cif_path)
+
 csv_data = pd.read_csv(csv_file)
-ehull_dict = {name: value for name, value in zip(csv_data["cif"], csv_data["ehull"])}
-energy_dict = {name: value for name, value in zip(csv_data["cif"], csv_data["energy"])}
-formula_dict = {
-    name: value for name, value in zip(csv_data["cif"], csv_data["formula"])
+dist_data = pd.read_csv(dist_file)
+property_data = pd.read_csv(property_file)
+
+csv_records = csv_data.to_dict(orient="records")
+dist_records = dist_data.to_dict(orient="records")
+property_records = property_data.to_dict(orient="records")
+
+csv_records_dict = {
+    data["cif"]: {k: v for k, v in data.items() if k != "cif"} for data in csv_records
+}
+dist_records_dict = {
+    data["file"].replace(".cif", ""): {k: v for k, v in data.items() if k != "file"}
+    for data in dist_records
+}
+property_records_dict = {
+    data["cif"]: {k: v for k, v in data.items() if k != "cif"}
+    for data in property_records
 }
 
 random.seed(42)
 random.shuffle(cif_names)
 
 split = [0.9, 0.05, 0.05]
+result_data = defaultdict(list)
 
-
-structures = []
-ehulls = []
-energys = []
-formulas = []
-material_id = []
-
-zs = []
 for cif_name in tqdm.tqdm(cif_names):
     if not cif_name.endswith(".cif"):
         continue
 
-    # if cif_name != 'In2Co2Te5_164_8419.vasp.cif':
-    #     continue
-    # import pdb;pdb.set_trace()
-
-    ehull = ehull_dict[cif_name.replace(".cif", "")]
+    key = cif_name.replace(".cif", "")
+    ehull = csv_records_dict[key]["ehull"]
     if ehull > 0.2:
         continue
     cif_file = os.path.join(cif_path, cif_name)
     parser = CifParser(cif_file)
     structure = parser.get_structures()[0]
+    sturcture = structure.to(fmt="cif")
 
-    # import pdb;pdb.set_trace()
-    # structure.lattice._matrix.setflags(write=True)
-    # # if structure.lattice._matrix[-1][-1] < 0:
-    # #     structure.lattice._matrix[-1][-1] += 25
-    # # else:
-    # #     structure.lattice._matrix[-1][-1] -= 25
-    # structure.lattice._matrix[-1][-1] /= 10
-    # structure.lattice._matrix.setflags(write=False)
+    result_data["material_id"].append(cif_name)
+    result_data["cif"].append(sturcture)
 
-    # coords = (structure.frac_coords - 0.3 )/0.4
-    # coords = coords.clip(0, 1)
-    # for i, site in enumerate(structure._sites):
-    #     site.c = coords[i, 2]
+    for csv_key in csv_data.keys():
+        if csv_key == "cif":
+            continue
+        try:
+            result_data[csv_key].append(csv_records_dict[key][csv_key])
+        except KeyError:
+            result_data[csv_key].append("")
 
-    # species = structure.species
-    # species = [s.name for s in species]
-    # new_structure = Structure(lattice=structure.lattice, species=species,
-    #     coords=coords,
-    #     )
-    # import pdb;pdb.set_trace()
-    # structure = new_structure
+    for dist_key in dist_data.keys():
+        if dist_key == "file":
+            continue
+        try:
+            result_data[dist_key].append(dist_records_dict[key][dist_key])
+        except KeyError:
+            result_data[dist_key].append("")
 
-    structures.append(structure.to(fmt="cif"))
-    ehulls.append(ehull)
-    energys.append(energy_dict[cif_name.replace(".cif", "")])
-    formulas.append(formula_dict[cif_name.replace(".cif", "")])
-    material_id.append(cif_name)
+    for property_key in property_data.keys():
+        if property_key in ["cif", "formula", "energy"]:
+            continue
+        try:
+            result_data[property_key].append(property_records_dict[key][property_key])
+        except KeyError:
+            result_data[property_key].append("")
 
-import pdb
+train_num = int(len(result_data["material_id"]) * split[0])
+val_num = int(len(result_data["material_id"]) * split[1])
+test_num = len(result_data["material_id"]) - train_num - val_num
 
-pdb.set_trace()
-
-train_num = int(len(material_id) * split[0])
-val_num = int(len(material_id) * split[1])
-test_num = len(material_id) - train_num - val_num
-
-train_data = {
-    "material_id": material_id[:train_num],
-    "energy": energys[:train_num],
-    "cif": structures[:train_num],
-    "ehull": ehulls[:train_num],
-    "formula": formulas[:train_num],
-}
+train_data = {key: value[:train_num] for key, value in result_data.items()}
 val_data = {
-    "material_id": material_id[train_num : train_num + val_num],
-    "energy": energys[train_num : train_num + val_num],
-    "cif": structures[train_num : train_num + val_num],
-    "ehull": ehulls[train_num : train_num + val_num],
-    "formula": formulas[train_num : train_num + val_num],
+    key: value[train_num : train_num + val_num] for key, value in result_data.items()
 }
-test_data = {
-    "material_id": material_id[-test_num:],
-    "energy": energys[-test_num:],
-    "cif": structures[-test_num:],
-    "ehull": ehulls[-test_num:],
-    "formula": formulas[-test_num:],
-}
+test_data = {key: value[-test_num:] for key, value in result_data.items()}
 
 df = pd.DataFrame(train_data)
 df.to_csv(os.path.join(save_path, "train.csv"))
