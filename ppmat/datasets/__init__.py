ee29f8ada5e4bf10822968dbae0fa765f10796ab
Yami <156195357+Le-soleile@users.noreply.github.com>
2025-12-08 19:14:27 +0800

    【PPMat Dataset No.6】OC20_CHGNet (#204)
    * add oc20_dataset
    
    * fix
    
    * Update README.md with new table entries
    
    * fix
    
    * fix
    
    * fix

--- ppmat/datasets/__init__.py
+++ ppmat/datasets/__init__.py
@@ -44,8 +44,7 @@ from ppmat.datasets.msd_nmr_dataset import MSDnmrinfos
 from ppmat.datasets.density_dataset import DensityDataset
 from ppmat.datasets.small_density_dataset import SmallDensityDataset
 from ppmat.datasets.num_atom_crystal_dataset import NumAtomsCrystalDataset
-from ppmat.datasets.tmqm_dataset import TmqmDataset
-from ppmat.datasets.omol25_dataset import OMol25Dataset
+from ppmat.datasets.oc20_s2ef_dataset import OC20S2EFDataset  # noqa
 from ppmat.datasets.split_mptrj_data import none_to_zero
 from ppmat.datasets.transform import build_transforms
 from ppmat.utils import logger
