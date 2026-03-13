9488d0bba4e5ecfaa257126ef7747c827377002a
leeleolay <805351297@qq.com>
2026-03-13 13:23:54 +0800

    Leeleolay patch 2 (#250)
    * Revise copyright and authorship statements
    
    Updated copyright information and authorship details.
    
    * Revise copyright and ownership details in mattergen.py
    
    Updated copyright notice and ownership information.
    
    * Update property_embeddings.py
    
    * Update copyright and ownership information in globals.py

--- ppmat/models/mattergen/globals.py
+++ ppmat/models/mattergen/globals.py
@@ -1,4 +1,13 @@
-# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+# 版权所有（C）2026 苏州国家实验室、百度飞桨团队
+# 本代码由苏州国家实验室与百度飞桨团队合作开发完成。
+# 其中，苏州国家实验室提供模型框架设计思路，百度飞桨团队负责代码实现。
+# 双方对本代码及相关成果享有同等权利。
+
+# Copyright (C) 2026 Suzhou National Laboratory and Baidu PaddlePaddle team
+# This code was jointly developed by Suzhou National Laboratory and Baidu PaddlePaddle team.
+# Suzhou National Laboratory provided the design concept for the model framework,
+# and Baidu PaddlePaddle team was responsible for the code implementation.
+# Both parties shall have equal rights to use this code and related work products.
 
 # Licensed under the Apache License, Version 2.0 (the License);
 # you may not use this file except in compliance with the License.
