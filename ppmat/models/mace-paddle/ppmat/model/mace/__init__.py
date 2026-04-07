40f577394eecc3265413c6fb079b2a1fa30e83b6
lsyt1 <153739214+lsyt1@users.noreply.github.com>
2026-04-07 22:02:20 +0800

    mace-paddle-2
    mace-paddle-2

---
+++ ppmat/models/mace-paddle/ppmat/model/mace/__init__.py
@@ -0,0 +1,5 @@
+from .model import MACE
+from .layers import EquivariantLayer, RadialBasisFunction
+from .utils import atomic_number_to_index
+
+__all__ = ['MACE', 'EquivariantLayer', 'RadialBasisFunction', 'atomic_number_to_index']
