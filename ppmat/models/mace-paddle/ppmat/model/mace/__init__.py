c6720237638c17d0f39dd95dd3107c270936c2b1
lsyt1 <153739214+lsyt1@users.noreply.github.com>
2026-04-23 16:20:58 +0800

    Delete ppmat/models/mace-paddle/ppmat/model/mace/__init__.py
    删除旧的mace-paddle目录文件

--- ppmat/models/mace-paddle/ppmat/model/mace/__init__.py
+++
@@ -1,5 +0,0 @@
-from .model import MACE
-from .layers import EquivariantLayer, RadialBasisFunction
-from .utils import atomic_number_to_index
-
-__all__ = ['MACE', 'EquivariantLayer', 'RadialBasisFunction', 'atomic_number_to_index']
